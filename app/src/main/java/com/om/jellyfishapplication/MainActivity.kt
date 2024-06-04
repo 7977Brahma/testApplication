2a661e90bb8cf87fefc22463a57eabdb643427e5
OPS@305 <omprasadsahu301@gmail.com>
2024-06-04 23:48:36 +0530

    Commit2

--- app/src/main/java/com/om/jellyfishapplication/MainActivity.kt
+++ app/src/main/java/com/om/jellyfishapplication/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.om.jellyfishapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.om.jellyfishapplication.ui.theme.JellyfishApplicationTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_scrollview)
 
         title="The Demo"
--- app/src/main/java/com/om/jellyfishapplication/NewActivity.kt
+++ app/src/main/java/com/om/jellyfishapplication/NewActivity.kt
@@ -1,6 +1,11 @@
 package com.om.jellyfishapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,10 +13,32 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class NewActivity : AppCompatActivity() {
+
+    lateinit var etUsername: EditText
+    lateinit var etPassword: EditText
+    lateinit var btnLogin: Button
+    lateinit var txtForgetPassword: TextView
+    lateinit var txtRegister: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
 
-        title="Log In"
+        title="Log IN"
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        txtForgetPassword = findViewById(R.id.txtForgetPassword)
+        txtRegister = findViewById(R.id.txtRegister)
+
+        btnLogin.setOnClickListener {
+            Toast.makeText(this@NewActivity,
+                "Login Successful",
+                Toast.LENGTH_LONG
+            ).show()
+            val intent = Intent(this@NewActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
